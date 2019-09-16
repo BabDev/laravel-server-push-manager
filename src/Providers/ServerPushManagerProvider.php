e62c2a8358afff75f92fb9aa0033fc253d3fd3d4
Michael Babker <michael.babker@gmail.com>
2019-09-15 21:30:24 -0500

    Make the service provider deferrable

--- src/Providers/ServerPushManagerProvider.php
+++ src/Providers/ServerPushManagerProvider.php
@@ -4,10 +4,23 @@ namespace BabDev\ServerPushManager\Providers;
 
 use BabDev\ServerPushManager\Contracts\PushManager as PushManagerContract;
 use BabDev\ServerPushManager\PushManager;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-final class ServerPushManagerProvider extends ServiceProvider
+final class ServerPushManagerProvider extends ServiceProvider implements DeferrableProvider
 {
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'babdev.push_manager',
+        ];
+    }
+
     /**
      * Register any application services.
      *
