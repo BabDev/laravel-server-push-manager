0dfc545f00305886ea391635852b0ae4bc050f75
mbabker <368545+mbabker@users.noreply.github.com>
2025-02-28 01:52:10 +0000

    Update facade docblocks

--- src/Facades/PushManager.php
+++ src/Facades/PushManager.php
@@ -6,14 +6,14 @@ use Illuminate\Support\Facades\Facade;
 use Psr\Link\EvolvableLinkProviderInterface;
 
 /**
- * @method static EvolvableLinkProviderInterface getLinkProvider()
- * @method static void                           setLinkProvider(EvolvableLinkProviderInterface $linkProvider)
- * @method static string                         link(string $uri, string|string[] $rel, array $attributes = [])
- * @method static string                         preload(string $uri, array $attributes = [])
- * @method static string                         dnsPrefetch(string $uri, array $attributes = [])
- * @method static string                         preconnect(string $uri, array $attributes = [])
- * @method static string                         prefetch(string $uri, array $attributes = [])
- * @method static string                         prerender(string $uri, array $attributes = [])
+ * @method static \Psr\Link\EvolvableLinkProviderInterface getLinkProvider()
+ * @method static void setLinkProvider(\Psr\Link\EvolvableLinkProviderInterface $linkProvider)
+ * @method static string link(string $uri, string|string[] $rel, array $attributes = [])
+ * @method static string preload(string $uri, array $attributes = [])
+ * @method static string dnsPrefetch(string $uri, array $attributes = [])
+ * @method static string preconnect(string $uri, array $attributes = [])
+ * @method static string prefetch(string $uri, array $attributes = [])
+ * @method static string prerender(string $uri, array $attributes = [])
  *
  * @see \BabDev\ServerPushManager\PushManager
  */
